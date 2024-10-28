08b418f57376ea1067b058d79088efb57b4a4855
tkkkd <>
2024-10-28 10:51:44 +0900

    ls receive str, String & update test

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,16 +1,10 @@
-// use std::io::{self, Write};
-use std::process::{Command, Output, Stdio};
-pub fn ls(args: &[String]) -> Output {
+use std::ffi::OsStr;
+use std::process::{Command, Output};
+pub fn ls<T: AsRef<OsStr> + ?Sized>(args: &[&T]) -> Output {
     let output = Command::new("ls")
         .args(args)
-        // .stdout(Stdio::piped())
-        // .stderr(Stdio::piped())
         .output()
         .expect("ls command failed to start");
 
-    // println!("{}", String::from_utf8_lossy(&output.stdout))
-    // println!("status: {}", output.status);
-    // io::stdout().write_all(&output.stdout).unwrap();
-    // io::stderr().write_all(&output.stderr).unwrap();
     output
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,26 @@
 pub mod cmd;
 
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::process::{Command, Output, Stdio};
 
     #[test]
-    fn it_works() {
-        let args = [String::from("-a")];
+    fn it_works_str() {
+        let args = ["-a"];
         let output = cmd::ls(&args);
 
         assert!(output.status.success());
-        // assert_eq!(
-        //     String::from_utf8_lossy(&output.stdout),
-        //     ".\n..\n.git\n.gitignore\nCargo.lock\nCargo.toml\nREADME.md\nsrc\ntarget\n"
-        // );
     }
 
+    #[test]
+    fn it_works_string() {
+        let args = [&String::from("-a")];
+        let output = cmd::ls(&args);
+
+        assert!(output.status.success());
+    }
     // #[test]
     // fn grep_ls() {
-    //     let ls_output = cmd::ls(&[]);
-
-    //     let output = Command::new("grep")
-    //         .arg("Cargo.toml")
-    //         .stdin(String::from_utf8_lossy(&ls_output.stdout))
-    //         .output();
-    //     assert!(output.status.success());
-    //     assert_eq!(String::from_utf8_lossy(&output.stdout), "Cargo.toml");
+    //     https://stackoverflow.com/questions/73469520/how-to-pipe-commands-in-rust
     // }
 }
